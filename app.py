7b23e63f27d623c7aca68123827a3c186e8cb372
KhanRana <47982799+KhanRana@users.noreply.github.com>
2023-08-26 16:34:41 +0100

    add all pages to the app.py file

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+from app_pages.multipage import MultiPage
 
-from app_pages.summary import summary_page 
-summary_page()
+# load pages scripts
+from app_pages.summary import summary_page
+from app_pages.findings import findings_page
+from app_pages.prediction import prediction_page
+from app_pages.hypothesis import hypothesis_page
+from app_pages.technical import performance_metrics_page
+
+app = MultiPage(app_name="Mildew Detector")  # Create an instance of the app
+
+# Add your app pages here using .add_page()
+app.add_page("Quick Project Summary", summary_page)
+app.add_page("Cells Visualiser", findings_page)
+app.add_page("Malaria Detection", prediction_page)
+app.add_page("Project Hypothesis", hypothesis_page)
+app.add_page("ML Performance Metrics", performance_metrics_page)
+
+app.run()  # Run the ap
 
 
