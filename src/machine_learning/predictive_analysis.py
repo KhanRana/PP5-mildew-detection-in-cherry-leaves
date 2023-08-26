bd7b1322a38e3f9eeebdfe109ce5dc03279c8352
KhanRana <47982799+KhanRana@users.noreply.github.com>
2023-08-26 15:51:17 +0100

    create load model function to predict image probability

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -7,6 +7,7 @@ from tf.keras.models import load_model
 from PIL import Image
 from src.data_management import load_pkl_file
 
+
 def resize_input_image(img, version):
     """
     Reshape image to 50x50 image size and normalise it
@@ -16,3 +17,24 @@ def resize_input_image(img, version):
     my_image = np.expand_dims(img_resized, axis=0)/255
 
     return my_image
+
+
+def load_model_and_predict(my_image, version):
+    """
+    Load and perform ML prediction over live images
+    """
+
+    model = load_model(f"outputs/{version}/mildew_detection_model.h5")
+
+    pred_proba = model.predict(my_image)[0, 0]
+
+    target_map = {v: k for k, v in {'healthy': 0, 'powdery_mildew': 1}.items()}
+    pred_class = target_map[pred_proba > 0.5]
+    if pred_class == target_map[0]:
+        pred_proba = 1 - pred_proba
+
+    st.write(
+        f"The predictive analysis indicates the sample cell is "
+        f"**{pred_class.lower()}** with malaria.")
+
+    return pred_proba, pred_class
