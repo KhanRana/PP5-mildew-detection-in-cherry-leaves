f0d9c0c40726a4bc14da2cb0052b545990bad2db
KhanRana <47982799+KhanRana@users.noreply.github.com>
2023-08-26 17:01:05 +0100

    remove Image.ANTIALIAS to fix the analysis error

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -13,7 +13,7 @@ def resize_input_image(img, version):
     Reshape image to 50x50 image size and normalise it
     """
     image_shape = load_pkl_file(file_path=f"outputs/{version}/image_shape.pkl")
-    img_resized = img.resize((image_shape[1], image_shape[0]), Image.ANTIALIAS)
+    img_resized = img.resize((image_shape[1], image_shape[0]))
     my_image = np.expand_dims(img_resized, axis=0)/255
 
     return my_image
